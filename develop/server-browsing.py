7e2165fc734c65d8a347f646761ed3fa2760b924
alllhvhla <alllhvhla@gmail.com>
2022-08-10 17:16:21 +0900

    chatbot page 구현

--- develop/server-browsing.py
+++ develop/server-browsing.py
@@ -18,7 +18,7 @@ params = {
 @app.route('/', methods=['GET'])
 def index():
     if request.method == "GET":
-        return render_template('main_layout.html', params=params, content="contents/main.html")
+        return render_template('main_layout.html', params=params, chatbot_talk="메인 페이지입니다.", content="contents/main.html")
 
 #%% Login
 
@@ -60,7 +60,7 @@ class User(UserMixin):
 
 @app.route('/login', methods=['GET'])
 def login():
-    return render_template('main_layout.html', params=params, content="login.html")
+    return render_template('main_layout.html', params=params, chatbot_talk="로그인 페이지입니다.", content="login.html")
 
 @app.route('/login', methods=["POST"])
 def loginCallback():
@@ -92,15 +92,15 @@ def logout():
 
 @app.route('/login/error')
 def login_error():
-    return render_template('main_layout.html', params=params, content="login.html", retry=True)
+    return render_template('main_layout.html', params=params, chatbot_talk="", content="login.html", retry=True)
 
 @app.route('/register', methods=['GET'])
 def register():
-    return render_template('main_layout.html', params=params, content="register.html")
+    return render_template('main_layout.html', params=params, chatbot_talk="회원가입 페이지입니다.", content="register.html")
 
 @app.route('/register/error')
 def register_error():
-    return render_template('main_layout.html', params=params, content="register.html", retry=True)
+    return render_template('main_layout.html', params=params, chatbot_talk="", content="register.html", retry=True)
 
 @app.route('/register', methods=["POST"])
 def register_callback():
@@ -132,7 +132,7 @@ def register_callback():
 
 @app.route('/forgot-password', methods=['GET'])
 def forgot_password():
-    return render_template('main_layout.html', params=params, content="forgot-password.html", user_info = -1)
+    return render_template('main_layout.html', params=params, chatbot_talk="비밀번호를 잊어버리셨나요?", content="forgot-password.html", user_info = -1)
 
 @app.route('/forgot-password/find-user', methods=['GET', 'POST'])
 def forgot_password_find_user():
@@ -147,24 +147,24 @@ def forgot_password_find_user():
     return render_template(
         'main_layout.html',
         content='forgot-password.html', 
-        params=params,
+        params=params, chatbot_talk="",
         user_info = user_info
     )
 
 #%% Dashboard    
 # @app.route('/dashboard', methods=['GET'])
 # def dashboard():
-#     return render_template('main_layout.html', params=params, content="contents/dashboard.html")
+#     return render_template('main_layout.html', params=params, chatbot_talk="", content="contents/dashboard.html")
 
 #%% Chart
 @app.route('/charts', methods=['GET'])
 def charts():
-    return render_template('main_layout.html', params=params, content="contents/charts.html")
+    return render_template('main_layout.html', params=params, chatbot_talk="", content="contents/charts.html")
 
 #%% Concept
 @app.route('/concept', methods=['GET'])
 def concept():
-    return render_template('main_layout.html', params=params, content="contents/concept.html")
+    return render_template('main_layout.html', params=params, chatbot_talk="", content="contents/concept.html")
     
 #%% Noticeboard
 @app.route('/noticeboard', methods=['GET'])
@@ -174,51 +174,51 @@ def noticeboard():
 # # 게시판 서버로 보낼 것
 # @app.route('/noticeboard/write', methods=['GET'])
 # def noticeboard_write():
-#     return render_template('main_layout.html', params=params, content="contents/noticeboard_write.html",
+#     return render_template('main_layout.html', params=params, chatbot_talk="", content="contents/noticeboard_write.html",
 #         table_contents=get_table_contents("free"), tag="free")
 
 # # 게시판 서버로 보낼 것
 # @app.route('/noticeboard/free', methods=['GET'])
 # def noticeboard_free():
-#     return render_template('main_layout.html', params=params, content="contents/noticeboard.html",
+#     return render_template('main_layout.html', params=params, chatbot_talk="", content="contents/noticeboard.html",
 #         table_contents=get_table_contents("free"), tag="free")
         
 # # 게시판 서버로 보낼 것
 # @app.route('/noticeboard/free/<int:i>', methods=['GET'])
 # def noticeboard_free_content(i):
-#     return render_template('main_layout.html', params=params, content="contents/noticeboard_content.html",
+#     return render_template('main_layout.html', params=params, chatbot_talk="", content="contents/noticeboard_content.html",
 #         title=str(i)+"번째 게시물", noticeboard_content=str(i)+"번째 본문")
 
 # # 게시판 서버로 보낼 것
 # @app.route('/noticeboard/review', methods=['GET'])
 # def noticeboard_review():
-#     return render_template('main_layout.html', params=params, content="contents/noticeboard.html",
+#     return render_template('main_layout.html', params=params, chatbot_talk="", content="contents/noticeboard.html",
 #         table_contents=[], tag="review")
 
 # # 게시판 서버로 보낼 것
 # @app.route('/noticeboard/tip', methods=['GET'])
 # def noticeboard_tip():
-#     return render_template('main_layout.html', params=params, content="contents/noticeboard.html",
+#     return render_template('main_layout.html', params=params, chatbot_talk="", content="contents/noticeboard.html",
 #         table_contents=[], tag="tip")
 
 # #%% Region
 # @app.route('/region', methods=['GET'])
 # def region():
-#     return render_template('main_layout.html', params=params, content="contents/region.html")
+#     return render_template('main_layout.html', params=params, chatbot_talk="", content="contents/region.html")
     
 # #%% Search
 # @app.route('/search', methods=['GET'])
 # def search():
-#     return render_template('main_layout.html', params=params, content="contents/search.html")
+#     return render_template('main_layout.html', params=params, chatbot_talk="", content="contents/search.html")
     
 # # @app.route('/votes', methods=['GET'])
 # # def votes():
-# #     return render_template('main_layout.html', params=params, content="contents/votes.html")
+# #     return render_template('main_layout.html', params=params, chatbot_talk="", content="contents/votes.html")
 
-# #%% Chatbot
-# @app.route('/chatbot', methods=['GET'])
-# def chatbot():
-#     return render_template('main_layout.html', params=params, content="blank.html")
+#%% Chatbot
+@app.route('/chatbot', methods=['GET'])
+def chatbot():
+    return render_template('main_layout.html', params=params, chatbot_talk="", content="chatbot.html")
 
 #%% App Start
 if __name__=="__main__":
